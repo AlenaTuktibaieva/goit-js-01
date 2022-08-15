5c4957a257df38cbc08173a59060c934efd813e0
AlenaTuktibaieva <alenatuktibaieva@gmail.com>
2022-08-15 21:30:16 +0200

    Update hw4.js

--- js/hw4.js
+++ js/hw4.js
@@ -29,19 +29,19 @@
 
 
 // Task 3
-// function makePizza(pizzaName, callback) {
-//   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
-//   callback(pizzaName);
-// }
+function makePizza(pizzaName, callback) {
+  console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+  callback(pizzaName);
+}
 
-// makePizza("Royal Grand", function deliverPizza(pizzaName) {
-//   console.log(`Delivering pizza ${pizzaName}.`);
-// });
-// // Change code below this line
+makePizza("Royal Grand", function deliverPizza(pizzaName) {
+  console.log(`Delivering pizza ${pizzaName}.`);
+});
+// Change code below this line
 
-// makePizza("Ultracheese", function eatPizza(pizzaName){
-//   console.log(`Eating pizza ${pizzaName}.`);
-// });
+makePizza("Ultracheese", function eatPizza(pizzaName){
+  console.log(`Eating pizza ${pizzaName}.`);
+});
 
 
 // Task 4
